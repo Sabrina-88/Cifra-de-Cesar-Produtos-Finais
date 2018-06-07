23e7b565f51c00d516642b491490892842d2701d
Sabrina Andrade <sabrina.andrade.leite@gmail.com>
2018-06-07 17:17:16 -0300

    app finalizado

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-var frase = prompt("Digite uma palavra: ");
-if (frase === ''|| frase >= 0 ){//nao pode deixar vazio nem numeros
-   alert("Atenção! Digite uma palavra: ");	
-  }else{
-var resultado = "";
-for (i = 0; i < frase.length; i++){
-  var cadaLetra;
-  var cifra; // criei esta variavel para poder puxar as funcoes que estao de fora
-  cadaLetra = frase[i].toUpperCase();
-   cifra = cipher(cadaLetra);
-  
-   resultado = resultado + cifra;
-  document.write(resultado); // nao dá pra usar alert
-}
-
-function cipher(cadaLetra){
-  var toAscii;
-  var result;
-  if(cadaLetra === frase[i]){ // são as maisuculas
-  toAscii = cadaLetra.charCodeAt();
-  var encoded;
-  encoded = (toAscii - 65 + 7)%26 + 65;
-  result = String.fromCharCode(encoded);console.log("MAIUS "+ result);
-  return result;
-}else{ // são as minuscula
-  toAscii = frase[i].charCodeAt();
-  var encoded;
-  encoded = (toAscii - 97 + 7)%26 + 97;
-  result = String.fromCharCode(encoded);console.log("min "+ result);
-  return result;
-  
-}
-}
-cipher(cadaLetra);
+var word = prompt("Digite uma palavra: ");
+if (word === ''|| word >= 0 || word <= 0){
+ alert("Atenção! Digite uma palavra: ");  
+}else{
+  var effect = "";
+  for (i = 0; i < word.length; i++){
+    var eachLetter;
+    var code; //Esta variavel é responsavel por receber o resultado da função
+    eachLetter = word[i].toUpperCase();
+    code = cipher(eachLetter);
+    effect = effect + code;
   }
- 
+
+  function cipher(eachLetter){
+    var toAscii;
+    var result;
+    var encoded;
+    if(eachLetter === word[i]){ //Essas são as letras maiúsculas
+      toAscii = eachLetter.charCodeAt();  
+      encoded = (toAscii - 65 + 7) % 26 + 65;
+      result = String.fromCharCode(encoded);
+      return result;
+    }else{ // são as minuscula
+      toAscii = word[i].charCodeAt();//Essas são as letras minúsculas
+      encoded = (toAscii - 97 + 7) % 26 + 97;
+      result = String.fromCharCode(encoded);
+      return result;
+    }
+  }
+  var response ="";
+  response += effect;
+  document.write("<p>Esta é sua palavra de maneira cifrada: " + response.bold() + "</p>");
+
+  cipher(eachLetter);
+  }
+  
